965d1f692b5db94e8bcb96611da32775087858d6
Lewis Truong <letruong@csumb.edu>
2020-11-06 15:55:52 -0800

    My changes

---
+++ Assets/Scripts/Inventory Scripts/Item Display Data/ItemDisplaySO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item Data", menuName = "Assets/Inventory/Item Data")]
+public class ItemDisplaySO : ScriptableObject
+{
+    public int value;
+    public GameObject itemLoot;
+}
---
+++ Assets/Scripts/Inventory Scripts/ItemDisplayInteractScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class ItemDisplayInteractScript : MonoBehaviour
+{
+    public ItemDisplaySO itemData;
+    public Transform displayCoord;
+    public Flowchart flowchart;
+    public string dialogue;
+    
+
+    void Start()
+    {
+        GameObject displayObject = Instantiate(itemData.itemLoot, displayCoord.position, Quaternion.identity);
+        displayObject.GetComponent<LootPickUpScript>().enabled = false;
+        displayObject.GetComponent<SphereCollider>().enabled = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.tag == "Player")
+        {                        
+            if(other.GetComponent<PlayerController>() != null)
+            {
+                other.GetComponent<PlayerController>().SetDisplay(this.gameObject);
+                flowchart.ExecuteBlock(dialogue);
+            }
+        }
+    }
+
+    //Returns name of item
+    public string getItemName() 
+    {
+        return itemData.itemLoot.name;
+    }
+
+    //Returns value of item
+    public int getValue()       
+    {
+        return itemData.value;
+    }
+
+    //Returns bool that represents if the player has more/exactly enough luppees compared
+    //to the value of the item.
+    public bool checkWallet()   
+    {
+        return PlayerController.lupeeCount >= itemData.value;
+    }
+
+    //Instantiates the item while subtracting its value from the player's luppee count
+    public void purchaseItem()
+    {
+        Instantiate(itemData.itemLoot, transform.position, Quaternion.identity);
+        PlayerController.lupeeCount -= itemData.value;
+    }
+}
